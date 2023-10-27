9c14596894d816601ff736ca97f1157d284dbdbc
GreetNeon <97176150+GreetNeon@users.noreply.github.com>
2023-10-27 12:26:06 +0000

    Changed parameter back to argv, and change function type

--- template/StepsTask1.c
+++ template/StepsTask1.c
@@ -58,10 +58,10 @@ int get_row_count(FILE* count_file){
 
 
 // Complete the main function
-void main(const char file_path){
+int main(int argc, char *argv[]){
     //Defining a File to be counted and a file to be formatted
-    FILE* count_file = fopen(file_path, "r"); 
-    FILE* input_file = fopen(file_path, "r");
+    FILE* count_file = fopen(argv[1], "r"); 
+    FILE* input_file = fopen(argv[1], "r");
     //Defining a count variable to add entries to the entries array
     //Using get_row_count to assign the amount of rows to a variable
     int row_count = get_row_count(count_file), count = 0;
@@ -90,6 +90,6 @@ void main(const char file_path){
         printf("%s/%s/%s", entries[2].date, entries[2].time, entries[2].steps);
     }
     
-    return;
+    return 0;
 }
 
