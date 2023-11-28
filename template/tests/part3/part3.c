49c54dba47d78e0cfa7ab2da32a3a2569dc33196
GreetNeon <97176150+GreetNeon@users.noreply.github.com>
2023-11-28 23:58:45 +0000

    Added validation to part 3

--- template/tests/part3/part3.c
+++ template/tests/part3/part3.c
@@ -40,18 +40,44 @@ Fitness_Data* read_from_file(char *file_loc){
     return entries;
 }
 
-void validate_time(char time[]){
-    char hour[3], minutes[3];
-    char *p = time;
-    const char *const_time = p;
+int validate_time(char time[]){
+    char hour[10], minutes[10];
+    char *temp_p = time;
+    const char *const_time = temp_p;
     tokenise_time(const_time, ":", hour, minutes);
-    printf("Hours: %s, minutes: %s\n", hour, minutes);
-    return;
+    if((-1<atoi(hour) && atoi(hour)<24) && (-1<atoi(minutes) && atoi(minutes)<61)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int validate_date(char date[]){
+    char year[10], month[10], day[10], *temp_p = date;
+    const char *const_date = temp_p;
+    tokeniseRecord(const_date, "-", year, month, day);
+    int year_i = atoi(year), month_i = atoi(month), day_i = atoi(day);
+    if((2000<year_i && year_i<3000) && (0<month_i && month_i<13) && (0<day_i && day_i<31)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+
+}
+int validate_steps(char steps[]){
+    int steps_i = atoi(steps);
+    if(-1<steps_i){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
 
-void main(){
+int main(){
     char file_path[1000];
-    int running = 1;
+    int running = 1, row_count, index;
     Fitness_Data *FitData;
     while(running == 1){
         printf("Enter Filename: ");
@@ -62,8 +88,24 @@ void main(){
         else{
             printf("The file was successfully opened!\n");
             FitData = read_from_file(file_path);
-            validate_time(FitData[0].time);
-        }
+            row_count = get_row_count(fopen(file_path, "r"));
+            printf("%d\n", validate_time(FitData[0].time));
+            printf("%d\n", validate_date(FitData[0].date));
+            printf("%d\n", validate_steps(FitData[0].steps));
+            int date_valid, time_valid, steps_valid;
+            for(index=0;index<row_count;index++){
+                date_valid = validate_date(FitData[index].date);
+                time_valid = validate_time(FitData[index].time);
+                steps_valid = validate_steps(FitData[index].steps);
+                if((date_valid == 0) || (time_valid == 0) || (steps_valid == 0)){
+                    printf("Data invalid!\n");
+                    return 0;
+                }
+                }
+            printf("All data is valid\n");
+            return 1;
+            }
     }
 }
+
 
